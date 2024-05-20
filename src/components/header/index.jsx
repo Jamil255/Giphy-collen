432afb852eb30d8287ddb950e91e0d5bddeed84e
unknown <jamilafzal255@gmail.com>
2024-05-21 01:13:30 +0500

    done

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from 'react-icons/hi2'
 import { Link } from 'react-router-dom'
+import { GifState } from '../../context/gif-context'
+import GifSearch from '../searchbar'
 
 const Header = () => {
   const [categories, setCategories] = useState([])
   const [showCategories, setShowCategories] = useState(false)
+  const { gf, filter, setFilter, favorites } = GifState()
+
+  const fetchGifCategory = async () => {
+    try {
+      const res = await fetch('/categories.json')
+      const { data } = await res.json()
+      setCategories(data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchGifCategory()
+  }, [])
+
   return (
     <nav>
       <div className="relative flex gap-4 justify-between items-center mb-2">
@@ -15,18 +33,26 @@ const Header = () => {
           </h1>
         </Link>
         <div className="font-bold text-md flex gap-2 items-center">
-          <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
-            Reactions
-          </Link>
+          {categories?.slice(0, 5)?.map((category) => {
+            return (
+              <Link
+                key={category?.name}
+                to={`${category?.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
+                {category?.name}
+              </Link>
+            )
+          })}
           <button onClick={() => setShowCategories(!showCategories)}>
             <HiEllipsisVertical
               size={35}
               className={`px-0.5 hover:gradient${
-                showCategories ? 'gradient' : ''
+                showCategories ? ' gradient' : ''
               } border-b-4 hidden lg:block`}
             />
           </button>
-          <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+          <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded hidden lg:block">
             <Link to={'favorites'}>Favorites GIFs</Link>
           </div>
           <button>
@@ -38,14 +64,25 @@ const Header = () => {
         </div>
         {showCategories && (
           <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
-            <span>Categories</span>
-            <hr />
-            <div>
-              <Link className="font-bold">Reactions</Link>
+            <span className="text-3xl font-extrabold ">Categories</span>
+            <hr className="bg-gray-100 opacity-50 my-5" />
+            <div className="grid grid-cols-2 sm:grid-cols-4  lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {categories?.map((category) => {
+                return (
+                  <Link
+                    key={category.name}
+                    to={`${category.name_encoded}`}
+                    className="font-bold"
+                  >
+                    {category.name}
+                  </Link>
+                )
+              })}
             </div>
           </div>
         )}
       </div>
+      <GifSearch filter={filter} setFilter={setFilter} />
     </nav>
   )
 }
