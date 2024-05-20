9ab02d4b6e567021ded76e3f349c3783e0e9c811
unknown <jamilafzal255@gmail.com>
2024-05-21 01:09:48 +0500

    home page done

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { data } from 'autoprefixer'
+import React, { useEffect } from 'react'
+import { GifState } from '../../context/gif-context'
+import Gif from '../../components/gif/Gif'
+import FilterGif from '../../components/filterGif/Filter-Gif'
 
 const Home = () => {
-  return <div>homepage</div>
+  const { gf, gifs, setGifs, filter } = GifState()
+  const fetchTrendingGIFs = async () => {
+    try {
+      const { data } = await gf.trending({
+        limit: 20,
+        type: filter,
+        rating: 'g',
+      })
+      setGifs(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTrendingGIFs()
+  }, [filter])
+  return (
+    <div>
+      <img src="/banner.gif" className="mt-2 rounded w-full" />
+      <FilterGif showTrending />
+      <div className="columns-2 md:columns-3 lg:colums-4 xl:colums-5 gap-2">
+        {gifs?.map((gif) => {
+          return <Gif gif={gif} key={gif?.title} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
